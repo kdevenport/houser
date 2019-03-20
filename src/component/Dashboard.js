4c958d9109d3f35c2fabf139c7d05ee2fa7c6774
kdevenport <katie.l.devenport@gmail.com>
2019-03-19 18:49:32 -0600

    addedcomponents

---
+++ src/component/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import House from './House';
+
+class Dashboard extends Component {
+    render(){
+        return(
+            <div><House /></div>
+        )
+    }
+}
+
+export default Dashboard;
