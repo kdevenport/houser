e696c95b4bd308882ae5809d5593acd45b18f1ec
kdevenport <katie.l.devenport@gmail.com>
2019-03-21 17:53:52 -0600

    addedmorefunctions

--- server/index.js
+++ server/index.js
@@ -6,9 +6,17 @@ require('dotenv').config();
 const controller = require('./controller');
 
 const app = express();
-massive( process.env.CONNECTION_STRING ).then( dbInstance => app.set('db', dbInstance) );
+massive(CONNECTION_STRING).then((dbInstance) =>{
+    app.set('db', dbInstance);
+}).catch((error) =>{
+    console.log(error);
+})
 
 app.use(bodyParser.json());
+
+//Endpoints
+
+app.get('/api/houses', controller.getHouses);
 
 
 const PORT = process.env.SERVER_PORT || 4040;
--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 import House from './House';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Dashboard extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            houses:[]
+        }
+    }
+    componentDidMount(){
+        this.getHouses();
+    }
+    getHouses = () => {
+        axios.get('api/houses').then( response => {
+            this.setState({
+                houses: response.data
+            })
+        })
+    }
     render(){
+        const mappedHouses = this.state.houses.map((house, index) =>{
+            return <House />
+        })
         return(
            <Link to='/wizard' className="Wizard">
             <button>
