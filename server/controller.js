90ca0aa7d1ff7249ba974c646aec7207086a3642
kdevenport <katie.l.devenport@gmail.com>
2019-03-21 19:08:47 -0600

    addedsomestuff

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,10 @@
 module.exports = {
-    getHouses: (req, res) =>{
-        
+    getHouses: (req, res) => {
+        const db = req.app.get('db');
+        db.get_houses().then(response => {
+            res.status(200).send(response);
+        }).catch(err =>{
+            console.log('getHouses:',err)
+        })
     }
 }
