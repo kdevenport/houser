53bea4555d72d2f3228859022839563cab959549
kdevenport <katie.l.devenport@gmail.com>
2019-05-28 19:29:56 -0600

    addedsomestylingtohouse

--- src/component/House.js
+++ src/component/House.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
+import './House.css';
+
 function House(props){
     const {deleteHouse, house} = props;
 
     return(
-        <div>
+        <div className="House">
+            <div className= "house_box">
             <p>Property Name: {props.house.property_name}</p>
             <p>Address: {props.house.street_address}</p>
             <p>City: {props.house.city}</p>
             <p>State: {props.house.state_location}</p>
             <p>Zipcode: {props.house.zipcode}</p>
-            <button onClick={() => deleteHouse(house.id)}>Delete</button>
+            <button className="house_delete" onClick={() => deleteHouse(house.id)}>Delete</button>
+            </div>
         </div>
     )
 }
